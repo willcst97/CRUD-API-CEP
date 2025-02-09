ed86bd569fa5d14ee64fed1ca50dbc4d55445b0f
William Costa <w.williamcosta@gmail.com>
2025-02-09 16:34:49 -0300

    desisti

--- src/config/mongoConection.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-await mongoose.connect("mongodb://localhost:27017");
-//mongodb://127.0.0.1:27017
-let db = mongoose.connection
-localhost:27017
-
-export default db;
--- users.js
+++
@@ -1,7 +0,0 @@
-
-let users = [
-    { username: 'usuario1', password: 'senha1' },
-    { username: 'usuario2', password: 'senha2' }
-  ];
-  
-  export default users;
